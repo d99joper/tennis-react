15386bc34ac9420c3411ddc7acd4e2b41de959a1
d99joper <jonas@zooark.com>
2022-09-22 17:09:48 -0700

    changed auth

--- src/App.js
+++ src/App.js
@@ -1,94 +1,66 @@
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator, Menu, MenuItem, View, Link, Flex, Heading } from '@aws-amplify/ui-react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+  } from 'react-router-dom';
 import "@aws-amplify/ui-react/styles.css";
 import { API, Storage } from 'aws-amplify';
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
-import { listNotes } from './graphql/queries';
+ import Profile from './profile'
+import NoPage from "./NoPage";
 
-const initialFormState = { name: '', description: '' }
+
+function Home() {
+    return <Heading level={2}>Home</Heading>;
+  }
+  
+  function About() {
+    return <Heading level={2}>About</Heading>;
+  }
+  
+//   function Profile() {
+//     return <Heading level={2}>Profile</Heading>;
+//   }
 
 function App({ signOut }) {
-  const [notes, setNotes] = useState([]);
-  const [formData, setFormData] = useState(initialFormState);
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(notesFromAPI.map(async note => {
-      if (note.image) {
-        const image = await Storage.get(note.image);
-        note.image = image;
-      }
-      return note;
-    }))
-    setNotes(apiData.data.listNotes.items);
-  }
-
-  async function createNote() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    if (formData.image) {
-      const image = await Storage.get(formData.image);
-      formData.image = image;
-    }
-    setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
-  }
-
-  async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
-  }
-
-  async function onChange(e) {
-    if (!e.target.files[0]) return
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file.name });
-    await Storage.put(file.name, file);
-    fetchNotes();
-  }
+  
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
-        value={formData.description}
-      />
-      <input
-        type="file"
-        onChange={onChange}
-      />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-      {
-        notes.map(note => (
-          <div key={note.id || note.name}>
-            <h2>{note.name}</h2>
-            <p>{note.description}</p>
-            <button onClick={() => deleteNote(note)}>Delete note</button>
-            {
-              note.image && <img src={note.image} style={{width: 400}} />
-            }
-          </div>
-        ))
-      }
-      </div>
-      <button onClick={signOut}>Sign Out</button>
+        <View width="4rem">
+            <Router>
+            
+                <Menu size="large">
+                    <MenuItem>
+                        <Link href="/">Home</Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <Link href="/profile">My Profile</Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <Link href="/about">About</Link>
+                    </MenuItem>
+                </Menu>
+            
+                {/* <Flex>
+                    <ReactRouterLink to="/" component={Link}>Home</ReactRouterLink>
+                    <ReactRouterLink to="/about" component={Link}>About</ReactRouterLink>
+                    <ReactRouterLink to="/users" component={Link}>Users</ReactRouterLink>
+                </Flex> */}
+
+                <Routes>
+                    <Route path="/about" element={<About />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/" element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+                </Routes>
+            </Router>
+        </View>
+
+    <button onClick={signOut}>Sign Out</button>
     </div>
   );
 }
-
-export default withAuthenticator(App);
+  export default withAuthenticator(App);
