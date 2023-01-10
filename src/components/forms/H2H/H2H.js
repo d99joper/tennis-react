48ce84cc1064e11d127a4e1487c1ee65e03e88ae
d99joper <jonas@zooark.com>
2023-01-09 23:32:52 -0800

    H2H finished

--- src/components/forms/H2H/H2H.js
+++ src/components/forms/H2H/H2H.js
@@ -4,36 +4,27 @@ import { SlUser } from "react-icons/sl";
 import './H2H.css'
 
 const H2H = ({ data, ...props }) => {
-    console.log(data)
+    
     return (
         data ?
             <Grid gap="0.1rem"
                 templateColumns="1fr 1fr 1fr"
             >
-
-                {/* inspiration
-                https://www.atptour.com/en/players/atp-head-2-head/mikael-ymer-vs-jannik-sinner/y268/s0ag
-                title: 
-                    pic p1    vs        pic p2
-                    name p1   XX-XX    name p2
-                
-                table:  
-                    stat p1   stat name     stat p2
-                
-                score list (ordered by playedOn)
-                date(year) ladder winner score */}
-                <Card columnStart={1} columnEnd={2} style={{ textAlign: 'center' }}>
-                   <View className={"profileImageContainer_100"}>
-                            {data.player1.image ?
-                                <Image
-                                    src={data.player1.imageUrl}
-                                    alt={`visual aid for ${data.player1.name}`}
-                                    className={`profileImage`}
-                                />
-                                : <SlUser size='100' />}
+                {/*** Player 1 ****/}
+                <Card columnStart={1} columnEnd={2} className="center">
+                    <View className={"profileImageContainer_100"}>
+                        {data.player1.image ?
+                            <Image
+                                src={data.player1.imageUrl}
+                                alt={`visual aid for ${data.player1.name}`}
+                                className={`profileImage`}
+                            />
+                            : <SlUser size='100' />}
                     </View>
                     <Text marginLeft={'.25rem'}>{data.player1.name}</Text>
                 </Card>
+
+                {/*** vs ****/}
                 <Card columnStart={2} columnEnd={3}>
                     <Grid gap="0" templateColumns={"1fr 0fr 1fr"}>
                         <span style={{ textAlign: 'center' }}>
@@ -42,16 +33,18 @@ const H2H = ({ data, ...props }) => {
                             </View>
                             <Text variation="success">{data.stats.matches.percentage} % wins</Text>
                         </span>
-                        <Divider orientation="vertical" textAlign={'center'} marginLeft="1rem" marginRight={'1rem'} />
+                        <Divider orientation="vertical" marginLeft="1rem" marginRight={'1rem'} />
                         <span style={{ textAlign: 'center' }}>
-                        <View className="circle_60 player2">
-                            <Text fontSize={'2.3rem'}>{data.stats.matches.losses}</Text>
-                        </View>
+                            <View className="circle_60 player2">
+                                <Text fontSize={'2.3rem'}>{data.stats.matches.losses}</Text>
+                            </View>
                             <Text>{100 - data.stats.matches.percentage} % wins</Text>
                         </span>
                     </Grid>
                 </Card>
-                <Card columnStart={3} columnEnd={-1} style={{ textAlign: 'center' }}>
+                
+                {/*** Player 2 ****/}
+                <Card columnStart={3} columnEnd={-1} className="center">
                     <View className={"profileImageContainer_100"}>
                         {data.player2.image ?
                             <Image
@@ -63,6 +56,23 @@ const H2H = ({ data, ...props }) => {
                     </View>
                     <Text marginLeft={'.25rem'}>{data.player2.name}</Text>
                 </Card>
+                
+                {/*** Stats section ****/}
+                {[{ name: 'sets' }, { name: 'tiebreaks' }, { name: 'games' }].map((x,i) =>
+                    <React.Fragment key={i}>
+                        <Card columnStart={1} columnEnd={2} className="statsCard">
+                            {`${data.stats[x.name].wins}/${data.stats[x.name].total} (${data.stats[x.name].percentage}%)`}
+                        </Card>
+                        <Card columnStart={2} columnEnd={3} className="statsCard statsName">
+                            {x.name.charAt(0).toUpperCase()+x.name.slice(1)} Won
+                        </Card>
+                        <Card columnStart={3} columnEnd={-1} className="statsCard">
+                            {`${data.stats[x.name].losses}/${data.stats[x.name].total} (${100 - data.stats[x.name].percentage}%)`}
+                        </Card>
+                    </React.Fragment>
+                )}
+
+                {/*** All matches ****/}
                 <Table columnStart={1} columnEnd={-1}>
                     <TableHead>
                         <TableRow backgroundColor={'#555'} borderRadius="5px">
--- src/components/layout/Modal/modal.js
+++ src/components/layout/Modal/modal.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 import "./modal.css";
+import {IoMdCloseCircle} from 'react-icons/io'
 
 const Modal = props => {
 
@@ -27,6 +28,9 @@ const Modal = props => {
       <div className="modal" onClick={props.onClose}>
         <div className="modal-content" onClick={e => e.stopPropagation()}>
           <div className="modal-header">
+            <span className="closeX" onClick={props.onClose}>
+              <IoMdCloseCircle size={'1.5rem'} />
+            </span>
             <h4 className="modal-title">{props.title}</h4>
           </div>
           <div className="modal-body">{props.children}</div>
