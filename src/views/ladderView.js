e387482a38f474c46a481a13d8e9e179a67bae09
d99joper <jonas@zooark.com>
2023-01-18 11:29:40 -0800

    geo sync

--- src/views/ladderView.js
+++ src/views/ladderView.js
@@ -1,5 +1,30 @@
 import React from "react";
 //import { ladderFunctions as lf } from "helpers";
+import { createMap, createAmplifyGeocoder } from "maplibre-gl-js-amplify";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
+import "maplibre-gl-js-amplify/dist/public/amplify-geocoder.css"; // Optional CSS for Amplify recommended styling
+import { ConsoleLogger } from "@aws-amplify/core";
+
+async function initializeMap() {
+    // const el = document.createElement("div");
+    // el.setAttribute("id", "map");
+    // document.body.appendChild(el);
+//38.55811189268456, -121.7512721677313
+    const map = await createMap({
+        container: "map",
+        center: [-121.7512721677313,38.55811189268456], // [Longitude, Latitude]
+        zoom: 13,
+    })
+    console.log(map)
+    map.addControl(createAmplifyGeocoder());
+
+//     const geocoder = createAmplifyGeocoder();
+// document.getElementById("map").appendChild(geocoder.onAdd())
+}
+
+initializeMap();
 
 const LadderView = () => {
 
@@ -8,6 +33,7 @@ const LadderView = () => {
     
     return (
     <>
+        <div id="map" style={{height: '500px'}}></div>
         Ladders
     </>
     )
