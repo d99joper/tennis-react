2d975dd13d47394733688295fa1a3fe12074ad89
d99joper <jonas@zooark.com>
2023-05-15 10:34:00 -0700

    map fix?

--- src/views/searchPage.js
+++ src/views/searchPage.js
@@ -1,11 +1,12 @@
 import {React, useState} from "react"
 import LadderSearch from "./ladder/search"
 import { TabItem, Tabs } from "@aws-amplify/ui-react"
-import { Box, Button, TextField } from "@mui/material"
+import { Box, Button, Slider, TextField } from "@mui/material"
+import { enums } from "helpers"
 
 const SearchPage = (props) => {
 
-  const [state, setState] = useState()
+  const [level, setLevel] = useState()
 
   return (
     <Tabs 
@@ -20,6 +21,20 @@ const SearchPage = (props) => {
       <TabItem title="Search for players">
         {/* Search box for player */}
         <TextField placeholder="Name..."/> 
+        {/* <div className="form-group"> */}
+          <Slider
+              getAriaLabel={()=>'Level'}
+              label="Level"
+              min={2}  
+              max={6.5}  
+              step={0.5}
+              value={level}
+              onChange={(e) => setLevel(e.target.value)}
+              disableSwap
+              marks={enums.LevelMarks}
+              valueLabelDisplay="auto"
+          />
+      {/* </div> */}
         <Button variant="outlined">Go</Button>
 
         {/* View box with the search results */}
