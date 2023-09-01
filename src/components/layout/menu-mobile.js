d49fc2f91d2f4617f9dfdbcb1f0be22c2416a894
d99joper <jonas@zooark.com>
2023-09-01 10:25:17 -0700

    sync with desktop

--- src/components/layout/menu-mobile.js
+++ src/components/layout/menu-mobile.js
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from "react";
 import MuiAppBar from '@mui/material/AppBar';
 import { userFunctions } from '../../helpers'
-import { Box, CssBaseline, Divider, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, makeStyles, Popover, styled, SwipeableDrawer, Toolbar, Typography, useTheme } from '@mui/material';
+import { AppBar, Box, CssBaseline, Divider, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, makeStyles, Popover, styled, SwipeableDrawer, Toolbar, Typography, useTheme } from '@mui/material';
 import { Global } from '@emotion/react';
 import { BsLadder, BsSearch } from 'react-icons/bs'
 import { SlUser } from 'react-icons/sl';
-import { AiOutlineMenuFold, AiOutlineMenuUnfold, AiOutlineHome, 
-         AiOutlineQuestionCircle, AiOutlineMail, AiOutlineSetting, AiOutlineInfoCircle } from 'react-icons/ai'
-import {BiLogInCircle, BiLogOutCircle} from 'react-icons/bi'
+import {
+    AiOutlineMenuFold, AiOutlineMenuUnfold, AiOutlineHome,
+    AiOutlineQuestionCircle, AiOutlineMail, AiOutlineSetting, AiOutlineInfoCircle
+} from 'react-icons/ai'
+import { BiLogInCircle, BiLogOutCircle } from 'react-icons/bi'
 // import bannerImage from '../../images/banner_tennis.jpg'
 import bannerImage from '../../images/tennisbanner.png'
 //import './layout.css'
 import { GiWhistle } from 'react-icons/gi';
 
 const MobileMenu = (props) => {
-    console.log("mobileMenu")
+    console.log("mobileMenu props", props)
 
     const [open, setOpen] = useState(false)
-    //console.log(initialUserScreen)
-    const [anchorEl, setAnchorEl] = useState(null)
-    
+
     const DrawerHeader = styled('div')(({ theme }) => ({
         display: 'flex',
         alignItems: 'center',
@@ -30,7 +30,7 @@ const MobileMenu = (props) => {
         ...theme.mixins.toolbar,
         justifyContent: 'flex-end',
     }))
-    
+
     const handleDrawerOpenClose = () => {
         console.log('open-close', !open)
         setOpen(!open);
@@ -38,105 +38,115 @@ const MobileMenu = (props) => {
 
     return (
 
-        <>
-            <div className={'drawer-puller banner-hamburger-icon'} onClick={handleDrawerOpenClose} >
-                <IconButton >
-                    <AiOutlineMenuUnfold color='white' size="1.5rem" />
-                </IconButton>
-            </div>
-            <SwipeableDrawer
-                sx={{
-                    width: 200,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
+        <AppBar className={"banner"}>
+            <Toolbar>
+                <div className='banner-title'>
+                    <h1>My Tennis Space</h1>
+                </div>
+                <div className={'drawer-puller banner-hamburger-icon'} onClick={handleDrawerOpenClose} >
+                    <IconButton >
+                        <AiOutlineMenuUnfold color='white' size="1.5rem" />
+                    </IconButton>
+                </div>
+                <SwipeableDrawer
+                    sx={{
                         width: 200,
-                        boxSizing: 'border-box',
-                        backgroundColor: '#c8e6c9',
-                        overflow: 'hidden'
-                    },
-                }}
-                anchor='left'
-                // variant={"temporary"}
-                open={open}
-                onClose={(e) => setOpen(false) }
-                onOpen={(e) => setOpen(true) }
-            >
-                <DrawerHeader>
-
-                    <span className={'drawer-puller'} onClick={handleDrawerOpenClose} >
-                        {open ?
-                            <IconButton>
-                                <AiOutlineMenuFold size="1.5rem" />
-                            </IconButton>
-                            :
-                            <IconButton>
-                                <AiOutlineMenuUnfold size="1.5rem" />
-                            </IconButton>
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                            width: 200,
+                            boxSizing: 'border-box',
+                            backgroundColor: '#c8e6c9',
+                            overflow: 'hidden'
+                        },
+                    }}
+                    anchor='left'
+                    open={open}
+                    onClose={handleDrawerOpenClose}
+                    onOpen={(e) => setOpen(true)}
+                >
+                    <DrawerHeader>
+                        <span className={'drawer-puller'} onClick={handleDrawerOpenClose} >
+                            {open ?
+                                <IconButton>
+                                    <AiOutlineMenuFold size="1.5rem" />
+                                </IconButton>
+                                :
+                                <IconButton>
+                                    <AiOutlineMenuUnfold size="1.5rem" />
+                                </IconButton>
+                            }
+                        </span>
+                    </DrawerHeader>
+                    <Divider />
+                    <List onClick={handleDrawerOpenClose} sx={{
+                        minWidth: 0
+                    }} >
+                        <ListItemButton title="Home" component={Link} to="/">
+                            <ListItemIcon>
+                                <AiOutlineHome size={'1.5rem'} />
+                            </ListItemIcon>
+                            <ListItemText primary={open && 'Home'} />
+                        </ListItemButton>
+                        {props.isLoggedIn === true &&
+                            <ListItemButton title="My Profile" component={Link} to="/profile">
+                                <ListItemIcon>
+                                    <SlUser size={'1.5rem'} />
+                                </ListItemIcon>
+                                <ListItemText primary={open && 'My Profile'} />
+                            </ListItemButton>
                         }
-                    </span>
-                </DrawerHeader>
-                <Divider />
-                <List sx={{
-                    minWidth: 0
-                }} >
-                    <ListItemButton title="Home" component={Link} to="/">
-                        <ListItemIcon>
-                            <AiOutlineHome size={'1.5rem'}  />
-                        </ListItemIcon>
-                        <ListItemText primary={open && 'Home'} />
-                    </ListItemButton>
-                    {props.isLoggedIn === true &&
-                        <ListItemButton title="My Profile" component={Link} to="/profile">
+                        <ListItemButton title="Search" component={Link} to="/search">
                             <ListItemIcon>
-                            <SlUser size={'1.5rem'} />
+                                <BsSearch size={'1.5rem'} />
                             </ListItemIcon>
-                            <ListItemText primary={open && 'My Profile'} />
+                            <ListItemText primary={open && 'Search'} />
                         </ListItemButton>
-                    }
-                    <ListItemButton title="Search" component={Link} to="/search">
-                        <ListItemIcon>
-                            <BsSearch size={'1.5rem'} />
-                        </ListItemIcon>
-                        <ListItemText primary={open && 'Search'} />
-                    </ListItemButton>
-                    <ListItemButton title="FAQ" component={Link} to="/faq">
-                        <ListItemIcon>
-                            <AiOutlineQuestionCircle size={'1.5rem'} />
-                        </ListItemIcon>
-                        <ListItemText primary={open && 'FAQ'} />
-                    </ListItemButton>
-                    <ListItemButton title="Rules" component={Link} to="/rules">
-                        <ListItemIcon>
-                            <GiWhistle size={'1.5rem'} />
-                        </ListItemIcon>
-                        <ListItemText primary={open && 'Rules'} />
-                    </ListItemButton>
-                    {props.isLoggedIn !== true &&
-                        <ListItemButton title="Login" component={Link} to="/login">
+                        <ListItemButton title="FAQ" component={Link} to="/faq">
                             <ListItemIcon>
-                            <BiLogInCircle size={'1.5rem'} />
+                                <AiOutlineQuestionCircle size={'1.5rem'} />
                             </ListItemIcon>
-                            <ListItemText primary={open && 'Login'} />
+                            <ListItemText primary={open && 'FAQ'} />
                         </ListItemButton>
-                    }
-                    {props.isLoggedIn === true &&
-                        <ListItemButton title="Logout" className='cursorHand' onClick={userFunctions.signOut}>
+                        <ListItemButton title="Rules" component={Link} to="/rules">
                             <ListItemIcon>
-                            <BiLogOutCircle size={'1.5rem'} />
+                                <GiWhistle size={'1.5rem'} />
                             </ListItemIcon>
-                            <ListItemText primary={open && 'Logout'} />
+                            <ListItemText primary={open && 'Rules'} />
                         </ListItemButton>
-                    }{props.currentUser?.isAdmin === true &&
-                        <ListItemButton title="Admin" component={Link} to="/adminTasks">
-                            <ListItemIcon>
-                                <AiOutlineSetting size={'1.5rem'} />
-                            </ListItemIcon>
-                            <ListItemText primary={open && 'Admin'} />
-                        </ListItemButton> 
+                        {props.isLoggedIn !== true &&
+                            <ListItemButton title="Login" component={Link} to="/login">
+                                <ListItemIcon>
+                                    <BiLogInCircle size={'1.5rem'} />
+                                </ListItemIcon>
+                                <ListItemText primary={open && 'Login'} />
+                            </ListItemButton>
+                        }
+                        {props.isLoggedIn === true &&
+                            <ListItemButton title="Logout" className='cursorHand' onClick={userFunctions.signOut}>
+                                <ListItemIcon>
+                                    <BiLogOutCircle size={'1.5rem'} />
+                                </ListItemIcon>
+                                <ListItemText primary={open && 'Logout'} />
+                            </ListItemButton>
+                        }{props.currentUser?.isAdmin === true &&
+                            <ListItemButton title="Admin" component={Link} to="/adminTasks">
+                                <ListItemIcon>
+                                    <AiOutlineSetting size={'1.5rem'} />
+                                </ListItemIcon>
+                                <ListItemText primary={open && 'Admin'} />
+                            </ListItemButton>
+                        }
+                    </List>
+                </SwipeableDrawer>
+                <div className='banner-settings'>
+                    {props.isLoggedIn === true ?
+                        <BiLogOutCircle title="Logout" size={'1.5rem'} className="cursorHand" onClick={userFunctions.signOut} />
+                        :
+                        <BiLogInCircle title="Login" size={'1.5rem'} className="cursorHand" onClick={() => console.log('poop')} component="Link" to="/login" />
                     }
-                </List>
-            </SwipeableDrawer>
-        </>
+                </div>
+            </Toolbar>
+        </AppBar>
     );
 }
 
