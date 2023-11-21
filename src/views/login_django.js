5b2d14e9eb4b12752a6a6fb958ea5d9857c9a19b
d99joper <jonas@zooark.com>
2023-11-21 08:26:09 -0800

    login w dj-auth

--- src/views/login_django.js
+++ src/views/login_django.js
@@ -1,34 +1,42 @@
+import { Flex } from '@aws-amplify/ui-react';
 import { GoogleLogin } from '@react-oauth/google'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { authAPI, playerAPI } from 'api/services';
 
 function Login() {
 
-  const googleLogin = async (accesstoken) => {
-    let res = await fetch("https://mytennis-space.uw.r.appspot.com/rest-auth/google/", {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json',
-        // 'Authorization': 'Bearer my-token'
-      },
-      body: JSON.stringify({access_token: accesstoken})
-    })
-    console.log(res);
-    return await res.status;
-  };
+  
+  function userLogin(e) {
+    e.preventDefault()
+    const form = new FormData(document.getElementById('loginForm'))
+    const username = form.get("username")
+    const pwd = form.get("password")
+    console.log(username, pwd)
+    authAPI.login(username, pwd)
+
+  }
 
   return (
-    <GoogleOAuthProvider clientId='107267563456-g2ceh5s5t31a5veq8j203rr7qtd00l41.apps.googleusercontent.com'>
-      <GoogleLogin
-        onSuccess={credentialResponse => {
-          console.log(credentialResponse);
-          googleLogin(credentialResponse.credential)
-        }}
-        onError={() => {
-          console.log('Login Failed');
-        }}
-        useOneTap
-      />
-    </GoogleOAuthProvider>
+    <>
+      <GoogleOAuthProvider clientId='107267563456-g2ceh5s5t31a5veq8j203rr7qtd00l41.apps.googleusercontent.com'>
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+            authAPI.googleLogin(credentialResponse.credential)
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+          useOneTap
+        />
+      </GoogleOAuthProvider>
+      <Flex id="loginForm" direction={'column'} as="form"
+        onSubmit={userLogin}>
+        Username: <input id="username" />
+        password: <input id="password" />
+        <input type='submit' value="Go"/>
+      </Flex>
+    </>
   )
 }
 
