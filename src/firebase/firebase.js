fec0298d516a0c77dc7ab41a9f513ae5dcf9a60b
d99joper <jonas@zooark.com>
2025-01-21 18:49:48 -0800

    notifications firebase service worker moved

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "mytennis-space.firebaseapp.com",
   projectId: "mytennis-space",
-  //storageBucket: "mytennis-space.firebasestorage.app",
+  storageBucket: "mytennis-space.firebasestorage.app",
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
